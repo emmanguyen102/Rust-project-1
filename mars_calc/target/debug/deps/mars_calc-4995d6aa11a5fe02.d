462f58ab9868520b74d9f2b6668937ed013ff8ce
Hang Nguyen <hang.nguyen@fortumcos.com>
2023-10-05 18:49:21 +0300

    Build a simple weight calculator

---
+++ mars_calc/target/debug/deps/mars_calc-4995d6aa11a5fe02.d
@@ -0,0 +1,5 @@
+/Users/hang.nguyen/Rust-project-1/mars_calc/target/debug/deps/mars_calc-4995d6aa11a5fe02: src/main.rs
+
+/Users/hang.nguyen/Rust-project-1/mars_calc/target/debug/deps/mars_calc-4995d6aa11a5fe02.d: src/main.rs
+
+src/main.rs:
---
+++ mars_calc/target/debug/mars_calc.d
@@ -0,0 +1,1 @@
+/Users/hang.nguyen/Rust-project-1/mars_calc/target/debug/mars_calc: /Users/hang.nguyen/Rust-project-1/mars_calc/src/main.rs
